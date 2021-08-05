cb56878e381ee05771721b52ac762cce5d2b571e
Hitalo Alves <hitalo.ralves@outlook.com>
2021-08-05 20:41:11 -0300

    Adicionando menu-toggle e responsividade ao header

--- frontend/src/components/templates/MenuToggle.jsx
+++ frontend/src/components/templates/MenuToggle.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 import './MenuToggle.css';
 
 const MenuToggle = () => {
+
+    const menuToggle = document.querySelector('.menu-toggle');
+
+    const activateMenu = (e) => {
+       menuToggle.classList.toggle('active')
+    }
+
     return ( 
-        <div className="menu-toggle">
-            <div className="line"></div>
-            <div className="line"></div>
+        <div className="menu-toggle" onClick={activateMenu}>
             <div className="line"></div>
         </div>
      );
