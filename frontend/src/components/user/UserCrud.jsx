0d7cbadf35ed40a98967679bb9f791e8ab80654d
Hitalo Alves <hitalo.ralves@outlook.com>
2021-08-04 21:12:36 -0300

    Estiizando form de cadastro

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -19,7 +19,9 @@ const UserCrud = () => {
     useEffect(() => {
         const fetchUsers = async() => {
             const resp = await axios(baseUrl)
-            setUsers({list: resp.data})
+            setUsers({
+                currentUser:{name:'', email:''},
+                list: resp.data})
         };
 
         fetchUsers();
@@ -63,29 +65,63 @@ const UserCrud = () => {
             <form action="#">
                 <div className="row">
                     <div className="form-group">
-                        <label htmlFor="name">Nome</label>
-                        <input type="text" name="name" value={users.currentUser.name} id="name" onChange={updateField} />
+                        <label htmlFor="nome">Nome</label>
+                        <input type="text" name="name" value={users.currentUser.name} id="nome" onChange={updateField} placeholder="Digite o nome.." />
                     </div>
-                </div>
-                <div className="row">
+
                     <div className="form-group">
                         <label htmlFor="email">E-mail</label>
-                        <input type="text" name="email" value={users.currentUser.email} id="email" onChange={updateField} />
+                        <input type="text" name="email" value={users.currentUser.email} id="email" onChange={updateField} placeholder="Digite o e-mail.."/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="button-group">
-                        <button className="btn-save" onClick={save}>Salvar</button>
-                        <button className="btn-cancel" onClick={clear}>Cancelar</button>
+                        <button className="button btn-save" onClick={save}>Salvar</button>
+                        <button className="button btn-cancel" onClick={clear}>Cancelar</button>
                     </div>
                 </div>
             </form>
         )
     }
 
+    const renderRows = () => {
+        return users.list.map(user => {
+            return (
+                <tr key={user.id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button className="btn-edit">
+                            <i className="fa fa-pencil"></i>
+                        </button>
+                        <button className="btn-delete">
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </td>
+                </tr>
+            )
+        })
+    }
+
+    const renderTable = () => {
+        return (
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Ações</th>
+                </tr>
+                {renderRows()}           
+            </table>
+        )
+    }
+
     return ( 
         <Main tipoContent="users">
             {renderForm()}
+            {/* <hr /> */}
+            {renderTable()}
         </Main>
     );
 }
